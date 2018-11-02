a7515263423a983063fc1e049441ee02b96d3186
odessabear@gmail.com <odessabear@gmail.com>
2018-11-02 17:12:00 +0200

    polymorphism:worked with Music.java file/Added new subclass and random output of instruments.

--- src/polymorphism/music/Music.java
+++ src/polymorphism/music/Music.java
@@ -71,6 +71,18 @@ class Woodwind extends Wind {
     }
 }
 
+class Cello extends Stringed{
+    void play(Note note){
+        System.out.println("Cello.play()");
+    }
+    void adjust(){
+        System.out.println("Adjusting Cello");
+    }
+    public String toString(){
+        return "Cello";
+    }
+}
+
 class Brass extends Wind {
     void play(Note note) {
         System.out.println("Brass.play()");
@@ -87,9 +99,12 @@ class Brass extends Wind {
 
 public class Music {
 
+    private static RandomInstrumentGenerator generator = new RandomInstrumentGenerator();
+
     public static void tune(Instrument instrument) {
         instrument.play(Note.MIDDLE_C);
     }
+
 
     public static void tuneAll(Instrument[] instruments) {
         for (Instrument instrument : instruments) {
@@ -98,16 +113,27 @@ public class Music {
     }
 
     public static void main(String[] args) {
-        Instrument[] orchestra = {
-                new Wind(),
-                new Percussion(),
-                new Stringed(),
-                new Brass(),
-                new Woodwind()
-        };
-        tuneAll(orchestra);
-        for (int i = 0; i < orchestra.length; i++) {
-            System.out.println(orchestra[i]);
+        Instrument [] instruments = new Instrument[15];
+
+//        Instrument[] orchestra = {
+//                new Wind(),
+//                new Percussion(),
+//                new Stringed(),
+//                new Brass(),
+//                new Woodwind(),
+//                new Cello()
+//        };
+//        tuneAll(orchestra);
+//        for (int i = 0; i < orchestra.length; i++) {
+//            System.out.println(orchestra[i]);
+//        }
+        for (int i = 0;i < instruments.length;i ++){
+            instruments[i] = generator.next();
         }
+
+        for (Instrument instrument:instruments){
+            instrument.adjust();
+        }
+
     }
 }
