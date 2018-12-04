fe1715b6c4a1f5a67f023d7a9bdfb95822f48709
odessabear@gmail.com <odessabear@gmail.com>
2018-12-04 19:10:36 +0200

    interfaces task 11 created lettersChanger method

--- src/interfaces/task11/ChangePosition.java
+++ src/interfaces/task11/ChangePosition.java
@@ -1,26 +1,35 @@
 package interfaces.task11;
 
-import interfaces.subinterface.Processor;
-
 import java.util.Arrays;
 
-public class ChangePosition extends CharChanger{
+public class ChangePosition extends CharChanger {
 
-    public static String toChangePosition(){
+    public static String toChangePosition() {
 
         return null;
     }
 
-    public String process(Object input){
-        return ((String)input).toLowerCase();
+    public String process(Object input) {
+        return ((String) input).toLowerCase();
+    }
+
+    public void lettersChanger(String s) {
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length-1; ) {
+            char temp = charArray[i];
+            charArray[i] = charArray[i + 1];
+            charArray[i + 1] = temp;
+            i = i+2;
+        }
+
+        System.out.println(Arrays.toString(charArray));
     }
 
 
+    public static void main(String[] args) {
+        ChangePosition changer = new ChangePosition();
+        String s = "ahahahah";
+        changer.lettersChanger(s);
 
-
-//    public static void main(String[] args) {
-//        String s = "go fuck away";
-//        char [] charArray = s.toCharArray();
-//        System.out.println(Arrays.toString(charArray));
-//    }
+    }
 }
