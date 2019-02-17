a86248677a3a3e80d452a168989a769f400ef20f
odessabear <bearodessa@gmail.com>
2019-02-17 22:58:53 +0200

    innerClasses - task2 done

---
+++ src/innerClasses/task1/task2/Sequence.java
@@ -0,0 +1,88 @@
+package innerClasses.task1.task2;
+
+interface Selector {
+    boolean end();
+
+    Object current();
+
+    void next();
+}
+
+class Strings {
+    private String word;
+
+     Strings(String s) {
+        word = s;
+    }
+
+    public String toString() {
+        return word;
+    }
+}
+
+public class Sequence {
+    private Object[] items;
+    private int next = 0;
+
+    public Sequence(int size) {
+        items = new Object[size];
+    }
+
+    public void add(Object x) {
+        if (next < items.length) {
+            items[next++] = x;
+        }
+    }
+
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+
+        @Override
+        public boolean end() {
+            return i == items.length;
+        }
+
+        @Override
+        public Object current() {
+            return items[i];
+        }
+
+        @Override
+        public void next() {
+            if (i < items.length) {
+                i++;
+            }
+        }
+    }
+    public Selector selector(){
+        return new SequenceSelector();
+    }
+
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for (int i =0;i<10;i++){
+            sequence.add(new Strings(Integer.toString(i)));
+        }
+
+        Selector selector = sequence.selector();
+
+        while (!selector.end()){
+            System.out.print(selector.current() + " ");
+            selector.next();
+        }
+        Strings s1 = new Strings("first");
+        Strings s2 = new Strings("second");
+        Strings s3 = new Strings("third");
+        Sequence sequence1 = new Sequence(3);
+        sequence1.add(s1);
+        sequence1.add(s2);
+        sequence1.add(s3);
+
+        Selector selector1 = sequence1.selector();
+        while (!selector1.end()){
+            System.out.print(selector1.current() +" ");
+            selector1.next();
+        }
+
+    }
+}
