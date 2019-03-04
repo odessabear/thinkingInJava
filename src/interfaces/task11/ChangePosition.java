51b7f1bfc389cac61183128d529ec59e31c99683
odessabear@gmail.com <odessabear@gmail.com>
2019-03-04 19:55:27 +0200

    returned changes in the interfaces.task11

--- src/interfaces/task11/ChangePosition.java
+++ src/interfaces/task11/ChangePosition.java
@@ -4,23 +4,21 @@ import java.util.Arrays;
 
  class ChangePosition {
 
-     Object process(Object input) {
-         return new ChangePosition(){
+     String process(Object input) {
+        return ChangePosition.lettersChanger((String)input);
+    }
 
+    private static String lettersChanger(String s) {
+        char[] charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length-1; ) {
+            char temp = charArray[i];
+            charArray[i] = charArray[i + 1];
+            charArray[i + 1] = temp;
+            i = i+2;
+        }
 
-             private String lettersChanger(String s){
-                 char[] charArray = s.toCharArray();
-                 for (int i = 0; i < charArray.length - 1; ) {
-                     char temp = charArray[i];
-                     charArray[i] = charArray[i + 1];
-                     charArray[i + 1] = temp;
-                     i = i + 2;
-                 }
+        return new String(charArray);
 
-                 return new String(charArray);
-
-             }
-         };
-     }
+    }
 
 }
