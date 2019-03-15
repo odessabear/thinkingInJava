28e24b9ba021f837f07e162bec9ab3da5eebaa58
odessabear@gmail.com <odessabear@gmail.com>
2019-03-15 20:08:26 +0200

    task 23 done

--- src/innerClasses/task23/A.java
+++ src/innerClasses/task23/A.java
@@ -42,7 +42,7 @@ public class A {
         b.getMethods();
         System.out.println();
         System.out.println();
-        b.setNull(2);
+        b.setNull(3);
         b.getMethods();
     }
 }
