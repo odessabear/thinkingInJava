e66dc1af1a54f85fb03be04d150da4f3853906f2
odessabear@gmail.com <odessabear@gmail.com>
2019-03-25 19:38:33 +0200

    task 26 start

---
+++ src/innerClasses/task26/WithInner1.java
@@ -0,0 +1,12 @@
+package innerClasses.task26;
+
+public class WithInner1 {
+    class InnerClass1{
+        private int x;
+        private String name = "name ";
+        public InnerClass1(int x,String name){
+            this.x = x;
+            this.name = name;
+        }
+    }
+}
