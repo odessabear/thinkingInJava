d666855eda19601a6bf7308c85e39b5e699d0493
odessabear <bearodessa@gmail.com>
2019-05-03 00:31:51 +0300

    collections:task 15 was done

---
+++ src/collections/task15/ReferenceWithStack.java
@@ -0,0 +1,49 @@
+package collections.task15;
+
+import collections.codeFromBook.Stack;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class ReferenceWithStack {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        stack.push("U");
+        stack.push("n");
+        stack.push("c");
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        stack.push("e");
+        stack.push("r");
+        stack.push("t");
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        stack.push("a");
+        System.out.print(stack.pop());
+        stack.push("i");
+        System.out.print(stack.pop());
+        stack.push("n");
+        stack.push("t");
+        stack.push("y");
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        stack.push(" ");
+        System.out.print(stack.pop());
+        stack.push("r");
+        stack.push("u");
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        stack.push("l");
+        stack.push("e");
+        stack.push("s");
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+        System.out.print(stack.pop());
+
+    }
+}
+
+
